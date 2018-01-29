2c69856c9f2408b0231045b620394430d6aff688
Georgiy Ivanov <georgiy.work@yandex.ru>
2018-01-29 11:19:13 +0500

    Add eslint standart config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
+mix.browserSync(process.env.APP_URL);
+
 mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
